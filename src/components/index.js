dcaf8f2d816716ac185247f964c654e6a39055e4
M00ose <tyrone.jacalan@protonmail.com>
2023-03-28 20:22:55 +0800

    Added about page import/export

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,10 @@
 import Header from "./Header";
 import Nav from "./Nav";
 import Hero from "./Hero";
+import About from "./About";
 import BlogFeed from "./BlogFeed";
 import Opportunities from "./Opportunities";
 import Partners from "./Partners";
 import Footer from "./Footer";
 
-export { Header, Nav, Hero, Opportunities, Partners, BlogFeed, Footer };
+export { Header, Nav, Hero, About, Opportunities, Partners, BlogFeed, Footer };
