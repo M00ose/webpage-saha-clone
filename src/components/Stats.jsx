f6e9fa58c93cb514e25c4e0d8b878427cdb03eb7
M00ose <tyrone.jacalan@protonmail.com>
2023-03-29 13:11:51 +0800

    Initial commit

---
+++ src/components/Stats.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+const Stats = () => {
+  return (
+    <div class="bg-white py-24 sm:py-32">
+      <div class="mx-auto max-w-7xl px-6 lg:px-8">
+        <dl class="grid grid-cols-1 gap-y-16 gap-x-8 text-center lg:grid-cols-3">
+          <div class="mx-auto flex max-w-xs flex-col gap-y-4">
+            <dt class="text-base leading-7 text-gray-600">
+              Women entrepreneurs trained
+            </dt>
+            <dd class="order-first text-3xl font-semibold tracking-tight text-gray-900 sm:text-5xl text-primary">
+              1,109
+            </dd>
+          </div>
+
+          <div class="mx-auto flex max-w-xs flex-col gap-y-4">
+            <dt class="text-base leading-7 text-gray-600">
+              Clean water businesses
+            </dt>
+            <dd class="order-first text-3xl font-semibold tracking-tight text-gray-900 sm:text-5xl text-primary">
+              389
+            </dd>
+          </div>
+
+          <div class="mx-auto flex max-w-xs flex-col gap-y-4">
+            <dt class="text-base leading-7 text-gray-600">People served</dt>
+            <dd class="order-first text-3xl font-semibold tracking-tight text-gray-900 sm:text-5xl text-primary">
+              127,062
+            </dd>
+          </div>
+        </dl>
+      </div>
+    </div>
+  );
+};
+
+export default Stats;
