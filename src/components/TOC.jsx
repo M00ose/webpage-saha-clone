628d1a8ec7e04d99a009e7c7c6ad178914c76f8b
M00ose <tyrone.jacalan@protonmail.com>
2023-03-30 13:38:43 +0800

    Added content and styles

--- src/components/TOC.jsx
+++ src/components/TOC.jsx
@@ -1,28 +1,23 @@
 import React from "react";
 import { Button } from "flowbite-react";
 
+import { toc } from "../constants";
+
 const TOC = () => {
   return (
-    <div className="relative grid grid-cols-4 items-center gap-2 px-6 md:px-0 pt-14">
-      <div className="absolute left-5 font-serif uppercase text-2xl text-offBlack">
-        <h1>Theory of Change</h1>
+    <div className="relative grid grid-cols-4 items-center gap-2 px-6 md:px-0 py-14">
+      <div className="absolute top-10 left-5 font-serif uppercase text-3xl text-primary">
+        <h1 className="font-bold">Theory of Change</h1>
       </div>
-      <div className="flex flex-col gap-4 items-center justify-center bg-blue-200 mx-auto p-2 max-w-xs w-full">
-        <h1>a</h1>
-        <p></p>
-      </div>
-      <div className="flex flex-col gap-4 items-center justify-center bg-blue-200 mx-auto p-2 max-w-xs w-full">
-        <h1>a</h1>
-        <p></p>
-      </div>
-      <div className="flex flex-col gap-4 items-center justify-center bg-blue-200 mx-auto p-2 max-w-xs w-full">
-        <h1>a</h1>
-        <p></p>
-      </div>
-      <div className="flex flex-col gap-4 items-center justify-center bg-blue-200 mx-auto p-2 max-w-xs w-full">
-        <h1>a</h1>
-        <p></p>
-      </div>
+      {toc.map((item) => (
+        <div
+          key={item.title}
+          className="flex flex-col gap-4 items-center justify-start bg-blue-200 mx-auto px-2 py-6 min-h-[300px] rounded-lg shadow-lg h-fit max-w-xs w-full text-left md:text-center"
+        >
+          <h3 className="font-bold font-serif text-lg">{item.title}</h3>
+          <p>{item.text}</p>
+        </div>
+      ))}
     </div>
   );
 };
