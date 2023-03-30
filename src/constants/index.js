07fc3008dbba8cfffc4295dd1d81fe6233b94326
M00ose <tyrone.jacalan@protonmail.com>
2023-03-30 13:37:18 +0800

    Added toc

---
+++ src/constants/index.js
@@ -0,0 +1,18 @@
+export const toc = [
+  {
+    title: "Activities",
+    text: "Build partnerships, create Saha Businesses, provide safe drinking water, and coach and mentor women entrepreneurs.",
+  },
+  {
+    title: "Outputs",
+    text: "Achieve community buy-in, increased income for women and their families, and communities have access to safe drinking water.",
+  },
+  {
+    title: "Outcomes",
+    text: "Reduced cases of waterborne diseases, increased safe drinking water practices, and women have sufficient income to sustain their business.",
+  },
+  {
+    title: "Impact",
+    text: "Healthier communities and disease-free families, increased productivity and reduced school absences, increased roles for women at home.",
+  },
+];
