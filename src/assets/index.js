81225f757cf35d8a09f3f8ce8a941a78b74da6bd
M00ose <tyrone.jacalan@protonmail.com>
2023-03-31 17:39:09 +0800

    Added assets for entrepreneurs and partners

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,3 +1,53 @@
 import hero from "./hero.jpg";
 
-export { hero };
+import Samata from "./entrepreneurs/Samata.jpg";
+import Balamposo from "./entrepreneurs/Balamposo.jpg";
+import Damu from "./entrepreneurs/Damu.jpg";
+import Mariama from "./entrepreneurs/Mariama.jpg";
+import Mazara from "./entrepreneurs/Mazara.jpg";
+import Sana from "./entrepreneurs/Sana.jpg";
+import Zaba from "./entrepreneurs/Zaba.jpg";
+
+import allpeoplebehappy from "./partners/allpeoplebehappy.png";
+import cartier from "./partners/cartier.png";
+import dovetail from "./partners/dovetail.png";
+import ldscharities from "./partners/ldscharities.jpg";
+import mulago from "./partners/mulago.png";
+import nicandzoe from "./partners/nicandzoe.jpg";
+import ospreyfoundation from "./partners/ospreyfoundation.png";
+import pictetfoundation from "./partners/pictetfoundation.png";
+import ra5 from "./partners/ra5.png";
+import sdgphilanthropyplatform from "./partners/sdgphilanthropyplatform.png";
+import stavros from "./partners/stavros.jpg";
+import theadventureproject from "./partners/theadventureproject.jpg";
+import theermfoundation from "./partners/theermfoundation.jpg";
+import vitolfoundation from "./partners/vitolfoundation.png";
+import waterharvest from "./partners/waterharvest.jpg";
+import waterloo from "./partners/waterloo.jpg";
+
+export {
+  hero,
+  Samata,
+  Balamposo,
+  Damu,
+  Mariama,
+  Mazara,
+  Sana,
+  Zaba,
+  allpeoplebehappy,
+  cartier,
+  dovetail,
+  ldscharities,
+  mulago,
+  nicandzoe,
+  ospreyfoundation,
+  pictetfoundation,
+  ra5,
+  sdgphilanthropyplatform,
+  stavros,
+  theadventureproject,
+  theermfoundation,
+  vitolfoundation,
+  waterharvest,
+  waterloo,
+};
