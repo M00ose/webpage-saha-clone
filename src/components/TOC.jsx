5e562d1020c268034fedeb58bf094c102bc5d394
M00ose <tyrone.jacalan@protonmail.com>
2023-03-31 17:53:49 +0800

    Edited header position and card background

--- src/components/TOC.jsx
+++ src/components/TOC.jsx
@@ -5,20 +5,44 @@ import { toc } from "../constants";
 
 const TOC = () => {
   return (
-    <div className="relative grid grid-cols-4 items-center gap-2 px-6 md:px-0 py-14">
-      <div className="absolute top-10 left-5 font-serif uppercase text-3xl text-primary">
-        <h1 className="font-bold">Theory of Change</h1>
-      </div>
-      {toc.map((item) => (
-        <div
-          key={item.title}
-          className="flex flex-col gap-4 items-center justify-start bg-blue-200 mx-auto px-2 py-6 min-h-[300px] rounded-lg shadow-lg h-fit max-w-xs w-full text-left md:text-center"
-        >
-          <h3 className="font-bold font-serif text-lg">{item.title}</h3>
-          <p>{item.text}</p>
+    <>
+      <div className="relative grid grid-rows-4 md:hidden items-center gap-2 px-6 md:px-0 py-14">
+        <div className="absolute top-8 left-5 font-serif uppercase text-3xl text-offBlack">
+          <h2 className="font-bold text-4xl md:text-5xl xl:text-6xl">
+            Theory of Change
+          </h2>
         </div>
-      ))}
-    </div>
+        {toc.map((item) => (
+          <div
+            key={item.title}
+            className="flex flex-col gap-4 items-center justify-start bg-primary mx-auto px-4 py-6 h-full rounded-lg shadow-lg md:max-w-xs w-full text-left md:text-center text-white overflow-clip"
+          >
+            <h3 className="font-bold font-serif text-2xl text-offGrey">
+              {item.title}
+            </h3>
+            <p>{item.text}</p>
+          </div>
+        ))}
+      </div>
+      <div className="invisible md:visible relative grid grid-cols-4 items-center gap-2 px-6 md:px-0 py-14">
+        <div className="absolute top-0 left-5 font-serif uppercase text-3xl text-offBlack">
+          <h2 className="font-bold text-4xl md:text-5xl xl:text-6xl">
+            Theory of Change
+          </h2>
+        </div>
+        {toc.map((item) => (
+          <div
+            key={item.title}
+            className="flex flex-col gap-4 items-center justify-start bg-primary mx-auto px-4 py-6 h-full rounded-lg shadow-lg md:max-w-xs w-full text-left md:text-center text-white overflow-clip"
+          >
+            <h3 className="font-bold font-serif text-2xl text-offGrey">
+              {item.title}
+            </h3>
+            <p>{item.text}</p>
+          </div>
+        ))}
+      </div>
+    </>
   );
 };
 
