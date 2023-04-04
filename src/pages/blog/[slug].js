2b2d1c3401a3192ca5b8b653348c927aa05c57b4
M00ose <tyrone.jacalan@protonmail.com>
2023-04-04 22:46:00 +0800

    Added styles

--- src/pages/blog/[slug].js
+++ src/pages/blog/[slug].js
@@ -1,3 +1,6 @@
+import styles from "../../styles/Slug.module.css";
+import { Nav, Footer } from "../../components";
+
 import { getPost, getSlugList } from "@/services";
 
 export async function getStaticPaths() {
@@ -21,12 +24,19 @@ export async function getStaticProps({ params }) {
 
 export default function BlogPost({ post }) {
   return (
-    <main>
-      <div>
-        <h1>{post.title}</h1>
-        <h6>{post.datePublished}</h6>
+    <main className="flex flex-col items-center justify-start min-h-screen w-screen font-sans overflow-hidden">
+      <div className="w-full xs:w-4/5 max-w-screen xl:max-w-[1024px]">
+        <Nav />
+        <div className="py-12">
+          <h1 className="text-5xl">{post.title}</h1>
+          <h6 className="font-serif">Posted on {post.datePublished}</h6>
+        </div>
+        <div
+          className={styles.content}
+          dangerouslySetInnerHTML={{ __html: post.content.html }}
+        />
       </div>
-      <div dangerouslySetInnerHTML={{ __html: post.content.html }} />
+      <Footer />
     </main>
   );
 }
