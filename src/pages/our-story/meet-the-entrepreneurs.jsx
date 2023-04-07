6f4584d01102d88bfbd23377b84225f8da8afda4
M00ose <tyrone.jacalan@protonmail.com>
2023-04-07 14:35:28 +0800

    Added content

--- src/pages/our-story/meet-the-entrepreneurs.jsx
+++ src/pages/our-story/meet-the-entrepreneurs.jsx
@@ -1,14 +1,35 @@
 import React from "react";
+import Image from "next/image";
 
 import { Nav, Footer } from "../../components";
 
-const entrepreneurs = () => {
+import { getEntrepreneurs } from "@/services";
+
+export async function getStaticProps() {
+  const { entrepreneurs } = await getEntrepreneurs();
+  return {
+    props: {
+      entrepreneurs,
+    },
+    revalidate: 10,
+  };
+}
+
+const meetTheEntrepreneurs = ({ entrepreneurs }) => {
   return (
     <main className="flex flex-col items-center justify-start min-h-screen w-screen font-sans overflow-hidden">
       <div className="w-full xs:w-4/5 max-w-screen xl:max-w-[1024px]">
         <Nav />
         <div className="relative isolate px-6 pt-14 pb-14 lg:px-8 font-sans">
-          <h1>Financials</h1>
+          {entrepreneurs.map((entrepreneur) => (
+            <Image
+              key={entrepreneur.name}
+              src={entrepreneur.coverPhoto.url}
+              alt={entrepreneur.name}
+              height={entrepreneur.coverPhoto.height}
+              width={entrepreneur.coverPhoto.width}
+            />
+          ))}
         </div>
       </div>
       <Footer />
@@ -16,4 +37,4 @@ const entrepreneurs = () => {
   );
 };
 
-export default entrepreneurs;
+export default meetTheEntrepreneurs;
