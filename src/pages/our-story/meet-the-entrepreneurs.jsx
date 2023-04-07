380f9234ef480f48658d1767a4a8b937e16bbfa4
M00ose <tyrone.jacalan@protonmail.com>
2023-04-07 18:32:41 +0800

    Added styles and modal

--- src/pages/our-story/meet-the-entrepreneurs.jsx
+++ src/pages/our-story/meet-the-entrepreneurs.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
+import { useRouter } from "next/router";
 import Image from "next/image";
+import Link from "next/link";
 
 import { Nav, Footer } from "../../components";
+import { Modal } from "@/components/Modal";
+import { EntrepreneurCard } from "@/components/EntrepreneurCard";
 
 import { getEntrepreneurs } from "@/services";
 
@@ -15,20 +19,44 @@ export async function getStaticProps() {
   };
 }
 
-const meetTheEntrepreneurs = ({ entrepreneurs }) => {
+const MeetTheEntrepreneurs = ({ entrepreneurs }) => {
+  const [active, setActive] = useState("");
+  let router = useRouter();
   return (
     <main className="flex flex-col items-center justify-start min-h-screen w-screen font-sans overflow-hidden">
       <div className="w-full xs:w-4/5 max-w-screen xl:max-w-[1024px]">
         <Nav />
-        <div className="relative isolate px-6 pt-14 pb-14 lg:px-8 font-sans">
+        {router.query.image && (
+          <Modal onClose={() => router.push("")}>
+            <EntrepreneurCard image={router.query.image} data={active} />
+          </Modal>
+        )}
+
+        <div className="py-12 columns-5 gap-1">
           {entrepreneurs.map((entrepreneur) => (
-            <Image
+            <Link
               key={entrepreneur.name}
-              src={entrepreneur.coverPhoto.url}
-              alt={entrepreneur.name}
-              height={entrepreneur.coverPhoto.height}
-              width={entrepreneur.coverPhoto.width}
-            />
+              href={`/our-story/meet-the-entrepreneurs/?image=${entrepreneur.coverPhoto.url}`}
+            >
+              <div className="max-w-[200px] hover:rounded-lg hover:shadow-2xl hover:scale-125 hover:cursor-pointer overflow-clip">
+                <Image
+                  src={entrepreneur.coverPhoto.url}
+                  alt={entrepreneur.name}
+                  height={entrepreneur.coverPhoto.height}
+                  width={entrepreneur.coverPhoto.width}
+                  className="object-cover mb-1"
+                  onClick={() =>
+                    setActive({
+                      name: entrepreneur.name,
+                      origin: entrepreneur.origin,
+                      height: entrepreneur.coverPhoto.height,
+                      width: entrepreneur.coverPhoto.width,
+                      description: entrepreneur.description,
+                    })
+                  }
+                />
+              </div>
+            </Link>
           ))}
         </div>
       </div>
@@ -37,4 +65,4 @@ const meetTheEntrepreneurs = ({ entrepreneurs }) => {
   );
 };
 
-export default meetTheEntrepreneurs;
+export default MeetTheEntrepreneurs;
