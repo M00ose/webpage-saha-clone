1e8c986f25778347a678f04ea3b904cc07664084
M00ose <tyrone.jacalan@protonmail.com>
2023-04-14 17:39:45 +0800

    Added content

--- src/pages/what-we-do/our-approach.jsx
+++ src/pages/what-we-do/our-approach.jsx
@@ -1,7 +1,43 @@
 import React from "react";
 
 const Approach = (props) => {
-  return <div className={props.visibility}>approach</div>;
+  return (
+    <div className={props.visibility}>
+      <div className="space-y-4">
+        <p>
+          Saha Global provides access to safe drinking water to rural
+          communities in Northern Region Ghana. We partner with rural
+          communities who lack access to safe drinking water, but do have access
+          to contaminated surface water sources. We then train local women to
+          use locally available products to treat the contaminated water and
+          make it safe to drink. The women then sell the clean water to their
+          community at an affordable price.
+        </p>
+        <p>
+          Saha donates the capital equipment needed to start each businesses,
+          but does not provide any financial support after the business is open.
+          This initial donation helps to keep the cost of running the business
+          low-enough that the entrepreneurs can charge their community, where
+          most people live on less than $2/day, affordable prices for their
+          services. Each business is completely owned and operated by the women
+          entrepreneurs and are financially sustainable.
+        </p>
+        <p>
+          Saha will not implement a new businesses unless we have raised the
+          funds for 10 years of monitoring and evaluation. Even including this
+          cost, our water businesses are a fraction of the cost of drilling a
+          well or borehore. Everyday, Our Customer Care team visits our partner
+          communities to check-in with the women entrepreneurs, offer businesses
+          advice and long-term training. We believe that our commitment to
+          monitoring, combined with our use of simple and locally available
+          technology, has been the{" "}
+          <a href="https://sahaglobal.org/what-we-do/sustainability/">
+            key to our long-term success in our partner communities.
+          </a>
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Approach;
--- src/pages/what-we-do/water-business.jsx
+++ src/pages/what-we-do/water-business.jsx
@@ -1,7 +1,177 @@
 import React from "react";
+import Image from "next/image";
+
+import styles from "@/styles";
+import { Timeline } from "flowbite-react";
 
 const WaterBusiness = (props) => {
-  return <div className={props.visibility}>waterBusiness</div>;
+  return (
+    <div className={`${props.visibility} space-y-4`}>
+      <p>
+        Saha trains women from rural villages how to launch small, clean water
+        businesses that provide safe water to their community at a price that
+        all can afford. For the past twelve years, Saha Global has worked
+        exclusively in the Northern Region of Ghana, where we have 389 water
+        businesses that provide safe drinking water to 127,062 people.
+      </p>
+      <h2 className={`${styles.header2} text-3xl font-extrabold`}>
+        Six Steps to Clean Drinking Water
+      </h2>
+      <h3 className={styles.header2}>1. Community Engagement</h3>
+      <p>
+        Saha works with local governments to identify villages that rely solely
+        on contaminated surface water sources. Local leaders nominate two to
+        five women to become Saha entrepreneurs to start a water treatment
+        business.
+      </p>
+      <h3 className={styles.header2}>2. Startup Toolkit</h3>
+      <p>
+        We provide each business with locally available materials to build a
+        water treatment center in the village, including a 1,000- liter
+        Polytank, a metal Polytank stand, and three blue 200-liter drums.
+      </p>
+      <h3 className={styles.header2}>3. Women&apos;s Training</h3>
+      <p>
+        Women learn to treat the water from contaminated sources to make it safe
+        to drink and to run the water business, including pricing, frequency of
+        treatment, and social marketing.
+      </p>
+      <h3 className={styles.header2}>4. Community Education</h3>
+      <p>
+        Saha visits every household in the village to educate families about
+        clean water, explain the water business to them, and distribute safe
+        storage containers (SSCs) that prevent water re-contamination in the
+        home.
+      </p>
+      <h3 className={styles.header2}>5. Water Treatment</h3>
+      <p>
+        Women entrepreneurs fetch dirty water from the dugout, fill up the
+        drums, and treat the water with aluminum sulfate that causes sediments
+        to settle to the bottom of the drums, leaving the water clear. They
+        transfer the clear water into the Polytank where they disinfect it with
+        chlorine. They sell the treated water for a price that everyone in the
+        village can afford. Villagers then store the treated water in SSCs.
+      </p>
+      <h3 className={styles.header2}>6. Customer Care</h3>
+      <p>
+        Women entrepreneurs receive ongoing mentoring and business advice from
+        Saha until they can run their business independently. We monitor water
+        quality and consumption in the village for 10 years – the amount of time
+        it takes for us to collect solid, accurate data.
+      </p>
+      <p>
+        Click through the picture gallery below to learn more about the
+        treatment processes currently in use at the water businesses in Ghana.
+      </p>
+      <h2 className={`${styles.header2} text-3xl font-extrabold`}>
+        How It Works
+      </h2>
+      <Timeline>
+        <Timeline.Item>
+          <Timeline.Point />
+          <Timeline.Content>
+            <Image
+              key="timeline-1"
+              src="/water-business/pic-about-water-businesses-01.jpg"
+              alt="women fetching water"
+              width={500}
+              height={500}
+            />
+            <Timeline.Body>
+              First, women fetch water from their local surface water source,
+              called a dugout. In most rural communities in northern Ghana,
+              people have no other option but to to drink this fecally
+              contaminated water.
+            </Timeline.Body>
+          </Timeline.Content>
+        </Timeline.Item>
+
+        <Timeline.Item>
+          <Timeline.Point />
+          <Image
+            key="timeline-2"
+            src="/water-business/pic-about-water-businesses-02.jpg"
+            alt="women fetching water"
+            width={500}
+            height={500}
+          />
+          <Timeline.Content>
+            <Timeline.Body>Text here</Timeline.Body>
+          </Timeline.Content>
+        </Timeline.Item>
+
+        <Timeline.Item>
+          <Timeline.Point />
+          <Timeline.Content>
+            <Timeline.Body>Text here</Timeline.Body>
+          </Timeline.Content>
+        </Timeline.Item>
+
+        <Timeline.Item>
+          <Timeline.Point />
+          <Timeline.Content>
+            <Timeline.Body>Text here</Timeline.Body>
+          </Timeline.Content>
+        </Timeline.Item>
+
+        <Timeline.Item>
+          <Timeline.Point />
+          <Timeline.Content>
+            <Timeline.Body>Text here</Timeline.Body>
+          </Timeline.Content>
+        </Timeline.Item>
+
+        <Timeline.Item>
+          <Timeline.Point />
+          <Timeline.Content>
+            <Timeline.Body>Text here</Timeline.Body>
+          </Timeline.Content>
+        </Timeline.Item>
+
+        <Timeline.Item>
+          <Timeline.Point />
+          <Timeline.Content>
+            <Timeline.Body>Text here</Timeline.Body>
+          </Timeline.Content>
+        </Timeline.Item>
+
+        <Timeline.Item>
+          <Timeline.Point />
+          <Timeline.Content>
+            <Timeline.Body>Text here</Timeline.Body>
+          </Timeline.Content>
+        </Timeline.Item>
+
+        <Timeline.Item>
+          <Timeline.Point />
+          <Timeline.Content>
+            <Timeline.Body>Text here</Timeline.Body>
+          </Timeline.Content>
+        </Timeline.Item>
+
+        <Timeline.Item>
+          <Timeline.Point />
+          <Timeline.Content>
+            <Timeline.Body>Text here</Timeline.Body>
+          </Timeline.Content>
+        </Timeline.Item>
+
+        <Timeline.Item>
+          <Timeline.Point />
+          <Timeline.Content>
+            <Timeline.Body>Text here</Timeline.Body>
+          </Timeline.Content>
+        </Timeline.Item>
+
+        <Timeline.Item>
+          <Timeline.Point />
+          <Timeline.Content>
+            <Timeline.Body>Text here</Timeline.Body>
+          </Timeline.Content>
+        </Timeline.Item>
+      </Timeline>
+    </div>
+  );
 };
 
 export default WaterBusiness;
